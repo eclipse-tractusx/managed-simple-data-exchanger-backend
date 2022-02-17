f7adecd485b8c732626159417922b9c93265f410
Gins Cyriac (ext.) <Gins.Cyriac@ctw.bmwgroup.com>
2022-02-17 14:49:16 +0000

    inital commit of login page

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,10 @@
+import Login from "./pages/login"
+
 function App() {
-  return (
-    <div className="text-center">
-      <header className="bg-header min-h-screen flex flex-col items-center justify-center text-secondary text-5xl">
-        <h1>Welcome to DFT!</h1>
-      </header>
-    </div>
-  );
+  return ( <div className="App">
+  <Login />
+
+  </div>)
 }
 
 export default App;

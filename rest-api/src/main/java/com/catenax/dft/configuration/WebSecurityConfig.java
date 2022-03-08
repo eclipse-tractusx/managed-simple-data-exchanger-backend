82d3c19b4034b52fb82bf010099e75ba01ff52c0
joao.pinto <k14Sxr4d10NMOaHik8xCnLsCDTf>
2022-03-08 15:14:41 +0000

    added cors support

---
+++ rest-api/src/main/java/com/catenax/dft/configuration/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.catenax.dft.configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+    }
+}

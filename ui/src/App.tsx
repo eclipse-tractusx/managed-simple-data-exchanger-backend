f29ad5ae84ed72bf8d70eebd98b41b06384bdb74
Pedro Almeida <almeida.pedromi@gmail.com>
2022-03-10 14:04:27 +0000

    table history

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,7 @@
 import Login from './pages/Login';
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoutes';
+import ProtectedRoute from './modules/ProtectedRoutes';
 import Dashboard from './pages/Dashboard';
 
 import { useLocalStorage } from './modules/LocalStorage';

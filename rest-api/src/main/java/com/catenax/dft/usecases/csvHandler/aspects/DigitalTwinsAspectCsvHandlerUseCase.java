e7f1f24abfcfaba3412ca2810eb8e16b8d0a6461
DiogoBoucaCTW <101419597+DiogoBoucaCTW@users.noreply.github.com>
2022-03-31 11:13:23 +0100

    change prefix uiid

--- rest-api/src/main/java/com/catenax/dft/usecases/csvHandler/aspects/DigitalTwinsAspectCsvHandlerUseCase.java
+++ rest-api/src/main/java/com/catenax/dft/usecases/csvHandler/aspects/DigitalTwinsAspectCsvHandlerUseCase.java
@@ -46,7 +46,7 @@ public class DigitalTwinsAspectCsvHandlerUseCase extends AbstractCsvHandlerUseCa
     public static final String SERIAL_PART_TYPIZATION_1_0_0 = "urn:bamm:com.catenax.serial_part_typization:1.0.0";
     public static final String SERIAL_PART_TYPIZATION_ID_SHORT = "serialPartTypization";
     public static final String ENDPOINT_PROTOCOL_VERSION = "1.0";
-    public static final String PREFIX = "urn:uuid:";
+    private static final String PREFIX = "urn:uuid:";
 
 
     private final DigitalTwinGateway gateway;

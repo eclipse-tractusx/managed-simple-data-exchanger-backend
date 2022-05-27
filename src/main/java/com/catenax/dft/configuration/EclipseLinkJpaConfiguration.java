a5a12f1a7b07f72c89ab2b368156f43a5235ec50
João Pinto <Joao.PD.Pinto@ctw.bmwgroup.com>
2022-05-27 14:30:30 +0100

    add missed license descriptor

--- src/main/java/com/catenax/dft/configuration/EclipseLinkJpaConfiguration.java
+++ src/main/java/com/catenax/dft/configuration/EclipseLinkJpaConfiguration.java
@@ -1,5 +1,19 @@
+/*
+ * Copyright 2022 CatenaX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.catenax.dft.configuration;
-
 
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.springframework.beans.factory.ObjectProvider;
@@ -15,12 +29,10 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-
-
 @Configuration
-public class EclipseLinkJpaConfiguration  extends JpaBaseConfiguration {
+public class EclipseLinkJpaConfiguration extends JpaBaseConfiguration {
 
-    protected EclipseLinkJpaConfiguration (DataSource dataSource, JpaProperties properties, ObjectProvider<JtaTransactionManager> jtaTransactionManager) {
+    protected EclipseLinkJpaConfiguration(DataSource dataSource, JpaProperties properties, ObjectProvider<JtaTransactionManager> jtaTransactionManager) {
         super(dataSource, properties, jtaTransactionManager);
     }
 

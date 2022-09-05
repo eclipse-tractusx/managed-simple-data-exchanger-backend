57446f6e52a0fac5152b78365fdf93ba3fe08645
Sachin Argade <91305578+sachinargade123@users.noreply.github.com>
2022-09-05 12:36:46 +0530

    Update FailureLogsUseCase.java

--- src/main/java/com/catenax/dft/usecases/logs/FailureLogsUseCase.java
+++ src/main/java/com/catenax/dft/usecases/logs/FailureLogsUseCase.java
@@ -22,14 +22,19 @@ import org.springframework.stereotype.Service;
 import com.catenax.dft.entities.database.FailureLogEntity;
 import com.catenax.dft.gateways.database.FailureLogRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class FailureLogsUseCase {
 
     @Autowired
     private FailureLogRepository repository;
 
     public void saveLog(FailureLogEntity entity) {
+    	log.error("Error in process: {}, {}", entity.getProcessId(), entity.getLog());
         repository.save(entity);
     }
 
 }
+

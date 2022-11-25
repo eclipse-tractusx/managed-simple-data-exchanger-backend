fdf2fd1679e6b7fb1e3a615af67bc23ee7875f36
Sachin Argade <sachin.argade@t-systems.com>
2022-11-25 17:19:51 +0530

    add new api for user role permission access

--- modules/sde-core/src/main/java/org/eclipse/tractusx/sde/core/controller/RoleManagementController.java
+++ modules/sde-core/src/main/java/org/eclipse/tractusx/sde/core/controller/RoleManagementController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.eclipse.tractusx.sde.core.service.RoleManagementService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +30,16 @@ public class RoleManagementController {
 	@GetMapping(value = "/role/{role}/permissions")
 	@PreAuthorize("hasPermission(#role,'read_role_permission')")
 	public List<String> getRolePermission(@PathVariable("role") String role) {
-		return roleManagementService.getRolePermission(role);
+		return roleManagementService.getRolePermission(List.of(role));
+
+	}
+
+	@GetMapping(value = "/user/role/permissions")
+	@PreAuthorize("hasPermission('','read_role_permission')")
+	public List<String> getAllRolePermissions() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		List<String> list = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+		return roleManagementService.getRolePermission(list);
 
 	}
 }

51517833f6944a3c41a3e879403f533b845baed0
Sachin Argade <sachin.argade@t-systems.com>
2023-01-21 02:34:36 +0530

    change string joiner

--- modules/sde-core/src/main/java/org/eclipse/tractusx/sde/configuration/SecurityConfig.java
+++ modules/sde-core/src/main/java/org/eclipse/tractusx/sde/configuration/SecurityConfig.java
@@ -1,3 +1,23 @@
+/********************************************************************************
+ * Copyright (c) 2022 T-Systems International GmbH
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 package org.eclipse.tractusx.sde.configuration;
 
 import java.util.Arrays;
@@ -31,11 +51,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true, prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-	private static final String[] PUBLIC_URL = { "/ping", "/*/public/**", "/api-docs/**","/swagger-ui/**", "*/swagger-ui/**",
-			"/actuator/health/readiness", "/actuator/health/liveness", "/v3/api-docs/**" };
+	private static final String[] PUBLIC_URL = { "/ping", "/*/public/**", "/api-docs/**", "/swagger-ui/**",
+			"*/swagger-ui/**", "/actuator/health/readiness", "/actuator/health/liveness", "/v3/api-docs/**" };
 
 	@Value("${keycloak.resource.clientid}")
 	private String resourceName;
@@ -113,10 +133,10 @@ public class SecurityConfig {
             .anyRequest().authenticated();
         // @formatter:on
 
-        http.headers().xssProtection().and()
-		.contentSecurityPolicy("default-src 'self'; script-src 'self' 'unsafe-inline'")
-		.and().httpStrictTransportSecurity().requestMatcher(AnyRequestMatcher.INSTANCE);
-        
+		http.headers().xssProtection().and()
+				.contentSecurityPolicy("default-src 'self'; script-src 'self' 'unsafe-inline'").and()
+				.httpStrictTransportSecurity().requestMatcher(AnyRequestMatcher.INSTANCE);
+
 		return http.build();
 	}
 

40e3203bd0a361b2e22e47e1bb36ed258fc64a02
dvasunin <dmitrii.vasiunin@t-systems.com>
2023-08-28 21:30:40 +0300

    Javadoc for TryUtils

--- modules/sde-core/src/main/java/org/eclipse/tractusx/sde/core/utils/TryUtils.java
+++ modules/sde-core/src/main/java/org/eclipse/tractusx/sde/core/utils/TryUtils.java
@@ -5,11 +5,25 @@ import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 
 public class TryUtils {
+
+    /***
+     * A Runnable which may throw an Exception
+     *
+     * @param <E>
+     */
     @FunctionalInterface
     public interface ThrowableAction<E extends Exception> {
         void run() throws E;
     }
 
+    /***
+     * Tries to execute a Callable passed as a first parameter.
+     *
+     * @param callable - function to execute which may throw an Exception
+     * @param onErr - code to be executed if an Exception happens
+     * @return Optional with result of execution or empty if Exception was thrown
+     * @param <V>
+     */
     @SuppressWarnings("CallToPrintStackTrace")
     public static <V> Optional<V> tryExec(Callable<V> callable, Consumer<Exception> onErr) {
         try {
@@ -29,10 +43,24 @@ public class TryUtils {
         };
     }
 
+    /***
+     * Try to execute a function given as a  first parameter {@link ThrowableAction}
+     * If it is executed normally then returns true. On failure - false.
+     *
+     * @param runnable - a function to run which may throw an Exception
+     * @param onErr - a Consumer to be executed if an Exception happens.Stacktrace is printed by default
+     * @return true on success and false if an Exception has been thrown
+     * @param <E>
+     */
     public static <E extends Exception> boolean tryRun(ThrowableAction<E> runnable, Consumer<Exception> onErr) {
         return tryExec(voidToBooleanAdapter(runnable), onErr).orElse(false);
     }
 
+    /***
+     *
+     * @return a function which ignores any given parameter
+     * @param <ANY>
+     */
     public static <ANY> Consumer<ANY> IGNORE() {
         return a -> {};
     }

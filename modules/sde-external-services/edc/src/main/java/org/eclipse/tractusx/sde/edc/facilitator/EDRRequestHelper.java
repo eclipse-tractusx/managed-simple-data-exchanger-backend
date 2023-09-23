d9ffd44dade59570efea782df5a979e954226487
Sachin Argade <sachin.argade@t-systems.com>
2023-09-23 17:28:47 +0530

    Update proxy request parameter

--- modules/sde-external-services/edc/src/main/java/org/eclipse/tractusx/sde/edc/facilitator/EDRRequestHelper.java
+++ modules/sde-external-services/edc/src/main/java/org/eclipse/tractusx/sde/edc/facilitator/EDRRequestHelper.java
@@ -71,12 +71,8 @@ public class EDRRequestHelper extends AbstractEDCStepsHelper {
 	public Object getDataFromProvider(EDRCachedByIdResponse authorizationToken, String downloadDataAs) {
 		Map<String, String> authHeader = new HashMap<>();
 		authHeader.put(authorizationToken.getAuthKey(), authorizationToken.getAuthCode());
-		if ("csv".equalsIgnoreCase(downloadDataAs))
-			downloadDataAs = "?type=" + downloadDataAs;
-		else
-			downloadDataAs = "";
 		return edrApiProxy.getActualDataFromProviderDataPlane(
-				new URI(authorizationToken.getEndpoint() + downloadDataAs), authHeader);
+				new URI(authorizationToken.getEndpoint()), downloadDataAs, authHeader);
 	}
 
 }

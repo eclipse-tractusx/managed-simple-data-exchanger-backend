4bf3500f9099e556c7a6984d0af134b243384e51
Sachin Argade <sachin.argade@t-systems.com>
2024-01-31 23:33:10 +0530

    table coloumn mapping fix

--- modules/sde-common/src/main/java/org/eclipse/tractusx/sde/common/entities/CommonPropEntity.java
+++ modules/sde-common/src/main/java/org/eclipse/tractusx/sde/common/entities/CommonPropEntity.java
@@ -23,41 +23,53 @@ package org.eclipse.tractusx.sde.common.entities;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Transient;
 import lombok.Data;
 
 @Data
 @MappedSuperclass
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommonPropEntity {
-	
+
+	@Transient
 	@JsonProperty(value = "row_number")
 	private Integer rowNumber;
 
+	@Column(name = "process_id")
 	@JsonProperty(value = "process_id")
 	private String processId;
-	
+
+	@Column(name = "shell_id")
 	@JsonProperty(value = "shell_id")
 	private String shellId;
-	
+
+	@Column(name = "sub_model_id")
 	@JsonProperty(value = "sub_model_id")
 	private String subModelId;
-	
+
+	@Column(name = "usage_policy_id")
 	@JsonProperty(value = "usage_policy_id")
 	private String usagePolicyId;
-	
+
+	@Column(name = "asset_id")
 	@JsonProperty(value = "asset_id")
 	private String assetId;
-	
+
+	@Column(name = "access_policy_id")
 	@JsonProperty(value = "access_policy_id")
 	private String accessPolicyId;
-	
+
+	@Column(name = "contract_defination_id")
 	@JsonProperty(value = "contract_defination_id")
 	private String contractDefinationId;
-	
+
+	@Column(name = "deleted")
 	@JsonProperty(value = "deleted")
 	private String deleted;
-	
+
+	@Column(name = "updated")
 	@JsonProperty(value = "updated")
 	private String updated;
 

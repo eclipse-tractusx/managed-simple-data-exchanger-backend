9170c3b10bdf9081cd1bce89050b5af8e00fec1c
ChetanT-System <chetan.parate@t-systems.com>
2024-02-01 10:55:38 +0530

    -  Changes in Batch entity

--- modules/sde-submodules/batch/src/main/java/org/eclipse/tractusx/sde/submodels/batch/entity/BatchEntity.java
+++ modules/sde-submodules/batch/src/main/java/org/eclipse/tractusx/sde/submodels/batch/entity/BatchEntity.java
@@ -20,24 +20,24 @@
 
 package org.eclipse.tractusx.sde.submodels.batch.entity;
 
-import org.eclipse.tractusx.sde.common.entities.CommonPropEntity;
+import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Table(name = "batch")
 @Entity
 @Data
-@EqualsAndHashCode(callSuper = false)
-public class BatchEntity extends CommonPropEntity {
+public class BatchEntity implements Serializable {
 
     @Id
     @Column(name = "uuid")
     private String uuid;
+    @Column(name = "process_id")
+    private String processId;
     @Column(name = "batch_id")
     private String batchId;
     @Column(name = "part_instance_id")
@@ -52,5 +52,21 @@ public class BatchEntity extends CommonPropEntity {
     private String classification;
     @Column(name = "name_at_manufacturer")
     private String nameAtManufacturer;
+    @Column(name = "shell_id")
+    private String shellId;
+    @Column(name = "sub_model_id")
+    private String subModelId;
+    @Column(name = "contract_defination_id")
+    private String contractDefinationId;
+    @Column(name = "usage_policy_id")
+    private String usagePolicyId;
+    @Column(name = "access_policy_id")
+    private String accessPolicyId;
+    @Column(name = "asset_id")
+    private String assetId;
+    @Column(name = "deleted")
+    private String deleted;
+    @Column(name = "updated")
+    private String updated;
     
 }

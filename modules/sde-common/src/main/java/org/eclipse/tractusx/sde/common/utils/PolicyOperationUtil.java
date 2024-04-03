6c7e70501a837dca1dcf222dfbd689d4e022e0c1
Sachin Argade <sachin.argade@t-systems.com>
2024-04-03 10:45:10 +0530

    Handle empty bpn for specific asset external sublect id

--- modules/sde-common/src/main/java/org/eclipse/tractusx/sde/common/utils/PolicyOperationUtil.java
+++ modules/sde-common/src/main/java/org/eclipse/tractusx/sde/common/utils/PolicyOperationUtil.java
@@ -21,6 +21,7 @@ package org.eclipse.tractusx.sde.common.utils;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.tractusx.sde.common.entities.Policies;
 import org.eclipse.tractusx.sde.common.entities.PolicyModel;
 
@@ -32,7 +33,7 @@ public class PolicyOperationUtil {
 		return policies
 				.stream()
 				.filter(e -> e.getTechnicalKey().equals("BusinessPartnerNumber"))
-				.flatMap(e -> e.getValue().stream())
+				.flatMap(e -> e.getValue().stream().filter(StringUtils::isNotBlank))
 				.toList();
 	}
 	
